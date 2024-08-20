714d4817eeb306452c4008c5e08b489ee01dd9f3
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-20 21:19:33 +0400

    Logo and reload image added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import i from './i.jpg';
+import r from './refresh-button.png';
+import './css/style.css'
+
+const logoImg = document.querySelector("#logo");
+const reloadImg = document.querySelector("#reload");
+reloadImg.src = r;
+logoImg.src = i;
