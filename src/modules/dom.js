f325ad1d781002d986acd4250469510f8e1d8e78
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-21 15:09:09 +0400

    selected the search field and search button

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,3 +1,10 @@
 const logoImg = document.querySelector("#logo");
 const reloadImg = document.querySelector("#reload");
-export {logoImg, reloadImg}
+const infoDiv = document.querySelector("#info");
+const address = infoDiv.querySelector(".info-1 span");
+const timezone = infoDiv.querySelector(".info-2 span");
+const description = infoDiv.querySelector(".info-3 span");
+const search = document.querySelector(".search");
+const searchBtn = document.querySelector(".search-btn");
+
+export { logoImg, reloadImg, address, timezone, description,search,searchBtn};
