f99ab9d00fdec15d5a5b1009efcb81a5bebe32d6
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-23 23:15:07 +0400

    fix typo

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -6,5 +6,23 @@ const timezone = infoDiv.querySelector(".info-2 span");
 const description = infoDiv.querySelector(".info-3 span");
 const search = document.querySelector(".search");
 const searchBtn = document.querySelector(".search-btn");
+const gif = document.querySelector("#gif");
+const currentWeatherDiv = document.querySelector("#current .Current");
+const todayWeatherDiv = document.querySelector("#today .today");
+const hourlyWeatherDiv = document.querySelector("#hourly .hourly");
+const dailyWeatherDiv = document.querySelector("#daily .Daily");
 
-export { logoImg, reloadImg, address, timezone, description,search,searchBtn};
+export {
+  logoImg,
+  reloadImg,
+  address,
+  timezone,
+  description,
+  search,
+  searchBtn,
+  gif,
+  currentWeatherDiv,
+  todayWeatherDiv,
+  hourlyWeatherDiv,
+  dailyWeatherDiv,
+};
