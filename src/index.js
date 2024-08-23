78bfb162261cf5323aa66e5f32884f0e460e5e00
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-23 23:13:59 +0400

    Update the timely weather data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
 import i from "./i.jpg";
 import r from "./refresh-button.png";
 import "./css/style.css";
+import { setupEventListeners } from "./modules/events.js";
+
 import {
   logoImg,
   reloadImg,
-  search,
-  searchBtn,
 } from "./modules/dom.js";
-import { apiCall } from "./modules/api.js";
 reloadImg.src = r;
 logoImg.src = i;
-let loc;
-searchBtn.addEventListener('click',(e) => {
-  e.preventDefault();
- loc =  search.value;
-apiCall(loc);
-});
 
+document.addEventListener('DOMContentLoaded',() => {
+  setupEventListeners();
+  const fixedTop = document.querySelector('.header');
+  const navLinks = document.querySelectorAll('ul a');
+
+  function adjustBodyPadding() {
+    const fixedHeight = fixedTop.offsetHeight;
+    document.body.style.paddingTop = fixedHeight + 'px';
+  }
+
+  window.addEventListener('load', adjustBodyPadding);
+  window.addEventListener('resize', adjustBodyPadding);
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      const targetId = this.getAttribute('href');
+      const targetElement = document.querySelector(targetId);
+      
+      if (targetElement) {
+        const fixedHeight = fixedTop.offsetHeight;
+        const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - fixedHeight;
+        
+        window.scrollTo({
+          top: targetPosition,
+          behavior: 'smooth'
+        });
+      }
+    });
+  });
+})
 
