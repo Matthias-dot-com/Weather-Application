46caead51a8de045b7162862193a9f04cf35a7ef
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-23 23:14:18 +0400

    Improved the api setup

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,20 +1,50 @@
-import { description,timezone,address } from "./dom.js";
-export async function apiCall(location) {
+import { gif } from "./dom.js";
+import {
+  API_KEY,
+  BASE_URL,
+  GIF_API,
+  GIF_BASE_URL,
+} from "./config/configuration.js";
+
+let condition = "overcast";
+export async function apiCall(location, unit) {
+  const url = `${BASE_URL}/${location}?unitGroup=${unit}&key=${API_KEY}&contentType=json`;
+
   try {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=J63FSDZHR5S7UK5NZE6TQSSPK`);
+    const response = await fetch(url);
 
     if (!response.ok) {
-      // If the response status is not ok, throw an error with the status text
-      throw new Error(`HTTP error! Status: ${response.status} ${response.statusText}`);
+      throw new Error(
+        `HTTP error! Status: ${response.status} ${response.statusText}`
+      );
     }
-
-    const data = await response.json();
-    description.innerHTML  = (data.description);
-    timezone.innerHTML = data.timezone;
-    address.innerHTML = data.address;
+    // condition = data.currentConditions.conditions;
+    // await getGif();
+    return response.json();
   } catch (error) {
-    // Catch and log any errors that occur during the fetch or JSON parsing
     console.error("Error during API call:", error);
   }
 }
 
+let Url;
+export async function getGif() {
+  const url = ` ${GIF_BASE_URL}?api_key=${GIF_API}s=${condition}&weirdness=4`;
+  try {
+    const resp = await fetch(url);
+    if (!resp.ok) {
+      throw new Error(`Http Error! status: ${resp.status}, ${resp.statusText}`);
+    }
+    const response = await resp.json();
+    Url = response.data.images.original.url;
+    const img = document.createElement("img");
+
+    img.src = Url;
+    gif.innerHTML = "";
+    gif.appendChild(img);
+    console.log(response.data.url);
+  } catch (error) {
+    console.log("error during loading api call", error);
+  }
+}
+
+export { Url };
