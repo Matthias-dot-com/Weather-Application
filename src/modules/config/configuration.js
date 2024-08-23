9e28102b36dbc1498e56f6b9c1d79fcb4adaac3f
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-08-23 23:20:07 +0400

    Just for the sake of cleanness

---
+++ src/modules/config/configuration.js
@@ -0,0 +1,6 @@
+const API_KEY = "J63FSDZHR5S7UK5NZE6TQSSPK";
+const BASE_URL ="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+const GIF_BASE_URL = "https://api.giphy.com/v1/gifs/translate";
+const GIF_API = "buwqXH5s0M6tXf4FJygF1ycd11yfeyqh";  
+export {API_KEY,BASE_URL,GIF_API,GIF_BASE_URL};
